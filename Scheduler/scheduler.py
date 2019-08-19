613f41aa2ce8c15f72043746e99fb32bb9a945e9
emkku <iamrock2360700@gmail.com>
2019-08-19 21:25:40 +0530

    Taking scheduler input

--- Scheduler/scheduler.py
+++ Scheduler/scheduler.py
@@ -11,13 +11,6 @@ cols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 df = pd.read_excel(xls, "Sheet1", usecols=cols)
 print(df)
 df = df[df.valid == 1]
-print(df)
-a = df[(df["sku"] == "TA012") & (df["depo"] == "AP01")]
-print(a, type(a))
-print(a[1], type(a[1]))
-i = 0
-print(type(df))
-sched = {}
 while i < 15:
     i += 1
     sched[i] = []
