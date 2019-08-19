6714e29de8411a8fa6a804425bd35c6d391167cf
emkku <iamrock2360700@gmail.com>
2019-08-19 20:37:40 +0530

    Proper arrangement

--- Scheduler/scheduler.py
+++ Scheduler/scheduler.py
@@ -1,10 +1,16 @@
-import pandas
+import pandas as pd
 import xlrd
+import os
+a=os.getcwd()
+a=a.split("\\Scheduler")
+os.chdir(a[0])
 global df
-xls = pd.ExcelFile(import_file_path)
-
+xls = pd.ExcelFile(os.getcwd()+"\\Output\\ttrucko.xlsx")
+cols=[0,1,2,3,4,5,6,7,8,9]
+df = pd.read_excel(xls,"Sheet1",usecols=cols)
+print(df)
+i=0,
 sched={}
-i=0
 while i<15:
     i+=1
     sched[i]=[]
